e6b4a65f11c79cdb46e525b33cacc4caafc34879
Saanicc <mattias470@gmail.com>
2025-03-25 23:14:59 +0100

    Update padding

--- src/components/Countries/Countries.tsx
+++ src/components/Countries/Countries.tsx
@@ -19,7 +19,7 @@ const Countries = ({
           <p>Loading...</p>
         </div>
       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 pb-4">
           {countries.map((country) => (
             <CountryCard key={country.name.common} country={country} />
           ))}
