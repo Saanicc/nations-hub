2df2802117609a664ebbdb1d69745ad209650f70
Saanicc <mattias470@gmail.com>
2025-04-11 20:11:11 +0200

    Show loading spinner when data is loading

--- src/components/Country/Country.tsx
+++ src/components/Country/Country.tsx
@@ -23,9 +23,10 @@ import Header from "../Header/Header";
 import BasicInfo from "./basic-info/BasicInfo";
 import CodesAndStatus from "./codes-status/CodesAndStatus";
 import InfoTabs from "./info-tabs";
+import { Spinner } from "@/components/ui/loading-spinner";
 
 const CountryPage = ({ id }: { id: string }) => {
-  const { data: country } = useQuery<Country>({
+  const { data: country, isLoading } = useQuery<Country>({
     queryKey: ["country", id],
     queryFn: async () => {
       const res = await fetch(`https://restcountries.com/v3.1/alpha/${id}`);
@@ -57,32 +58,42 @@ const CountryPage = ({ id }: { id: string }) => {
         </BreadcrumbList>
       </Breadcrumb>
       <Card className="h-full overflow-y-auto">
-        <CardHeader className="">
-          <div className="flex justify-between items-start">
-            <div>
-              <CardTitle className="text-2xl">{country.name.common}</CardTitle>
-              <CardDescription className="">
-                {country.name.official}
-              </CardDescription>
-            </div>
-            <Image
-              src={country.flags.png}
-              alt={country.flags.alt || `${country.name.common} flag`}
-              width={200}
-              height={200}
-              className="w-auto max-h-16 sm:max-h-20 md:max-h-32 lg:max-h-40 object-contain"
-              priority
-            />
+        {isLoading ? (
+          <div className="flex items-center justify-center h-full">
+            <Spinner className="">Loading...</Spinner>
           </div>
-        </CardHeader>
-        <CardContent className="pt-6">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-6">
-            <BasicInfo country={country} />
-            <CodesAndStatus country={country} />
-          </div>
+        ) : (
+          <>
+            <CardHeader className="">
+              <div className="flex justify-between items-start">
+                <div>
+                  <CardTitle className="text-2xl">
+                    {country.name.common}
+                  </CardTitle>
+                  <CardDescription className="">
+                    {country.name.official}
+                  </CardDescription>
+                </div>
+                <Image
+                  src={country.flags.png}
+                  alt={country.flags.alt || `${country.name.common} flag`}
+                  width={200}
+                  height={200}
+                  className="w-auto max-h-16 sm:max-h-20 md:max-h-32 lg:max-h-40 object-contain"
+                  priority
+                />
+              </div>
+            </CardHeader>
+            <CardContent className="pt-6">
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-6">
+                <BasicInfo country={country} />
+                <CodesAndStatus country={country} />
+              </div>
 
-          <InfoTabs country={country} />
-        </CardContent>
+              <InfoTabs country={country} />
+            </CardContent>
+          </>
+        )}
       </Card>
     </>
   );
